b61770bf16509b66b0ad7d04c74ab93140e6f0d5
Bastian B <bastian.berrios@sansano.usm.cl>
2023-11-26 21:45:59 -0300

    Apps added to settings

--- CER3/CER3/settings.py
+++ CER3/CER3/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #apps instaladas
+    'CER3',
+    'API',
 ]
 
 MIDDLEWARE = [
